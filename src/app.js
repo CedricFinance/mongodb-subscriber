059c5b940d99fea848d5ab9dc4c6e42f6a764560
Cédric Finance <cedric.finance@finandric.com>
2015-01-04 15:10:29 +0100

    Refactor activemqConnect and mongodbConnect

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-var Stomp = require('stomp-client');
 var Q = require('q');
 var config = require('./config');
 
-var client = new Stomp('mq', config.port, config.user, config.pass);
+function activemqConnect(mq_config) {
+  var Stomp = require('stomp-client');
+  var client = new Stomp(mq_config.alias, mq_config.port, mq_config.user, mq_config.pass);
 
-function activemqConnect(client) {
   var deferred = Q.defer();
   client.connect(function(sessionId) {
-    deferred.resolve(sessionId);
+    deferred.resolve(client);
   }).on('error', function(err) {
     deferred.reject(new Error(err));
   });
   return deferred.promise;
 }
 
-function mongodbConnect(url) {
+function mongodbConnect(mongo_config) {
   var MongoClient = require('mongodb').MongoClient;
   var connect = Q.denodeify(MongoClient.connect);
-  return connect(url);
+  return connect(mongo_config.url);
 }
 
-Q.all([activemqConnect(client), mongodbConnect(config.url)]).spread(function(sessionId, db) {
+Q.all([activemqConnect(config.activemq), mongodbConnect(config.mongo)]).spread(function(client, db) {
   console.log("Connected to activemq and mongodb");
 
   var collection = db.collection("raw_metrics");
 
-  client.subscribe(config.destination, function(body, headers) {
+  client.subscribe(config.activemq.destination, function(body, headers) {
     var metrics = JSON.parse(body);
     metrics.date = new Date(metrics.date);
     console.log('Message received:', metrics);
--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  destination: '/topic/electricity/metrics',
-  user: process.env.ACTIVEMQ_USER,
-  pass: process.env.ACTIVEMQ_PASSWORD,
-  port: 61613,
-  url: 'mongodb://mongo/electricity'
+  activemq: {
+    alias: 'mq',
+    user: process.env.ACTIVEMQ_USER,
+    pass: process.env.ACTIVEMQ_PASSWORD,
+    port: 61613,
+    destination: '/topic/electricity/metrics',
+  },
+  mongo: {
+    url: 'mongodb://mongo/electricity'
+  }
 };
